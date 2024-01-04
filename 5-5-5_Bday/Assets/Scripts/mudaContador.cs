effe1703b75ede5e9532da1c7215f48a61332807
p-mendoncad <125631065+p-mendoncad@users.noreply.github.com>
2024-01-04 20:35:15 -0300

    animação TextMeshPro Cena 2(jogo)

---
+++ 5-5-5_Bday/Assets/Scripts/mudaContador.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class mudaContador : MonoBehaviour
+{   
+    public TextMeshProUGUI Texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Texto = FindObjectOfType<TextMeshProUGUI>();
+    }
+
+private IEnumerator ShowContadorFuntion()
+        {
+            Texto.text = "INSPIRE\n0";
+            yield return new WaitForSeconds(1.5f);
+            Texto.text = "INSPIRE\n1";
+            yield return new WaitForSeconds(1f); 
+            Texto.text = "INSPIRE\n2";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "INSPIRE\n3";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "INSPIRE\n4";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "INSPIRE\n5";
+            yield return new WaitForSeconds(0.5f);
+            Texto.text = "SEGURE O AR\n0";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "SEGURE O AR\n1";
+            yield return new WaitForSeconds(1f); 
+            Texto.text = "SEGURE O AR\n2";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "SEGURE O AR\n3";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "SEGURE O AR\n4";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "SEGURE O AR\n5";
+            yield return new WaitForSeconds(0.5f);
+            Texto.text = "ENCHA O BALÃO\n0";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "ENCHA O BALÃO\n1";
+            yield return new WaitForSeconds(1f); 
+            Texto.text = "ENCHA O BALÃO\n2";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "ENCHA O BALÃO\n3";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "ENCHA O BALÃO\n4";
+            yield return new WaitForSeconds(1f);
+            Texto.text = "ENCHA O BALÃO\n5";
+            yield return new WaitForSeconds(0.5f);
+            Texto.text = " ";
+        }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+        StartCoroutine(ShowContadorFuntion());
+    }
+    }
+}
